887545479038c5cce9cc5f9203837ce7004f8d25
cbourjau <christian.bourjau@cern.ch>
2017-06-24 18:02:03 +0200

    A very low-key histogram

---
+++ histogram/src/lib.rs
@@ -0,0 +1,43 @@
+use std::f64::INFINITY;
+
+
+#[derive(Debug)]
+pub struct Histogram {
+    pub edges: Vec<f64>,
+    pub counts: Vec<f64>,
+}
+
+impl Histogram {
+    pub fn new(nbins: usize, min: f64, max: f64) -> Histogram {
+        let width = (max - min) / nbins as f64;
+        let edges: Vec<f64> =
+            vec![-INFINITY].into_iter()
+            .chain((0..nbins+1).map(|idx| {min + width * idx as f64}))
+            .chain(vec![INFINITY].into_iter())
+            .collect();
+        Histogram {
+            edges: edges,
+            counts: vec![0f64; nbins + 2],
+        }
+    }
+
+    pub fn fill(&mut self, value: f64) {
+        let idx = self.edges.iter().rposition(|e| {e <= &value}).expect("No bin found!");
+        self.counts[idx] += 1.0;
+    }
+}
+
+impl Extend<f64> for Histogram {
+    fn extend<T: IntoIterator<Item=f64>>(&mut self, values: T) {
+        for value in values {
+            self.fill(value);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
