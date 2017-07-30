78bb3f4b9464fe45e4daae516e1495a7883e4141
cbourjau <christian.bourjau@cern.ch>
2017-07-30 13:32:23 +0200

    chain analyses together

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,15 @@ fn main() {
         .map(|path| Dataset::new(path.unwrap().to_str().unwrap()))
         .flat_map(|ev| ev);
 
-    let mut pt_mult = analyses::PtMultiplicity::new();
-    let mut single_dists = analyses::SingleParticleDistributions::new();
-    let mut pair_dists = analyses::ParticlePairDistributions::new();
-    let mut event_dists = analyses::EventDistributions::new();
+    trait Analysis: ProcessEvent + Visualize {}
+    impl<T> Analysis for T where T: ProcessEvent + Visualize {}
+
+    let mut analyses: Vec<Box<Analysis>> = vec![
+        Box::new(analyses::PtMultiplicity::new()),
+        Box::new(analyses::SingleParticleDistributions::new()),
+        Box::new(analyses::ParticlePairDistributions::new()),
+        Box::new(analyses::EventDistributions::new()),
+        ];
 
     let mut hist_ntracks_v0 = HistogramBuilder::<Ix2>::new()
                 .add_equal_width_axis(10, 0., 2e3)
@@ -49,28 +54,23 @@ fn main() {
             let pv = ev.primary_vertex.as_ref().unwrap();
             // see AliESDtrackCuts.cxx:1366
             ev.tracks.iter()
-                .filter(|tr| {tr.flags.contains(track::ITS_REFIT)})
-                .filter(|tr| {tr.dca_to_point_xy(pv.x, pv.y) < 2.4})
-                .filter(|tr| {tr.dca_to_point_z(pv.z) < 3.2})
+                .filter(|tr| tr.flags.contains(track::ITS_REFIT))
+                .filter(|tr| tr.dca_to_point_xy(pv.x, pv.y) < 2.4)
+                .filter(|tr| tr.dca_to_point_z(pv.z) < 3.2)
                 .filter(|tr| tr.eta().abs() < 0.8)
                 .filter(|tr| tr.quality_tpc.ncls > 70)
                 .filter(|tr| tr.pt() > 0.15)
-                .map(|tr| tr)
                 .collect()
         };
 
         // Correlation between number of tracks and multiplicity
         let v0_mult = ev.vzero.multiplicity_v0a() + ev.vzero.multiplicity_v0c();
         hist_ntracks_v0.fill(&[v0_mult as f64, ev.multiplicity as f64]);
-
-        pt_mult.process_event(&ev, filtered_tracks.as_slice());
-        single_dists.process_event(&ev, filtered_tracks.as_slice());
-        event_dists.process_event(&ev, filtered_tracks.as_slice());
-        pair_dists.process_event(&ev, filtered_tracks.as_slice());
+        for analysis in analyses.iter_mut() {
+            (*analysis).process_event(&ev, filtered_tracks.as_slice());
+        }
     }
-
-    single_dists.visualize();
-    pt_mult.visualize();
-    event_dists.visualize();
-    pair_dists.visualize();
+    for analysis in analyses {
+        (*analysis).visualize();
+    }
 }
