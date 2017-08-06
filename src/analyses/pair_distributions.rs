c58792a191c2e50902002894800dc01f64662c9d
cbourjau <christian.bourjau@cern.ch>
2017-08-06 15:39:53 +0200

    added mulitplicity dimension

--- src/analyses/pair_distributions.rs
+++ src/analyses/pair_distributions.rs
@@ -1,4 +1,6 @@
 use std::f64::consts::PI;
+use std::f64::INFINITY;
+
 use rustfft::FFTplanner;
 use rustfft::num_complex::Complex;
 use rustfft::num_traits::Zero;
@@ -16,72 +18,75 @@ use super::nanmean;
 
 
 pub struct ParticlePairDistributions {
-    singles: Histogram<Ix3>,
-    pub pairs: Histogram<Ix5>,
-    event_counter: Histogram<Ix1>,
+    singles: Histogram<Ix4>,
+    pub pairs: Histogram<Ix6>,
+    event_counter: Histogram<Ix2>,
 }
 
 impl ParticlePairDistributions {
     pub fn new() -> ParticlePairDistributions {
         // eta, phi, z
-        let nphi = 20;
+        let nphi = 36;
         let neta = 16;
         let (nzvtx, zmin, zmax) = (8, -8., 8.);
+        let multiplicity_edges = vec![500., 1000., 2000., INFINITY];
         ParticlePairDistributions {
-            singles: HistogramBuilder::<Ix3>::new()
+            singles: HistogramBuilder::<Ix4>::new()
                 .add_equal_width_axis(neta, -0.8, 0.8)
                 .add_equal_width_axis(nphi, 0., 2. * PI)
                 .add_equal_width_axis(nzvtx, zmin, zmax)
+                .add_variable_width_axis(&multiplicity_edges)
                 .build().expect("Error building histogram"),
-            pairs: HistogramBuilder::<Ix5>::new()
+            pairs: HistogramBuilder::<Ix6>::new()
                 .add_equal_width_axis(neta, -0.8, 0.8)
                 .add_equal_width_axis(neta, -0.8, 0.8)
                 .add_equal_width_axis(nphi, 0., 2. * PI)
                 .add_equal_width_axis(nphi, 0., 2. * PI)
                 .add_equal_width_axis(nzvtx, zmin, zmax)
+                .add_variable_width_axis(&multiplicity_edges)
                 .build().expect("Error building histogram"),
-            event_counter: HistogramBuilder::<Ix1>::new()
+            event_counter: HistogramBuilder::<Ix2>::new()
                 .add_equal_width_axis(nzvtx, zmin, zmax)
+                .add_variable_width_axis(&multiplicity_edges)
                 .build().expect("Error building histogram"),
         }
     }
 
-    pub fn finalize(&self) -> nd::Array<f64, nd::Dim<[usize; 5]>> {
-        let nphi = 20;
-        let neta = 16;
-        let nzvtx = 8;
+    pub fn finalize(&self) -> nd::Array<f64, nd::Dim<[usize; 6]>> {
+        let shape = self.singles.counts.shape();
+        let (neta, nphi, nzvtx, nmult) = (shape[0], shape[1], shape[2], shape[3]);
         let phiphi = (&self.singles
                           .counts
                           .to_owned()
-                          .into_shape((1, neta, 1, nphi, nzvtx))
+                          .into_shape((1, neta, 1, nphi, nzvtx, nmult))
                           .expect("Can't reshape")
-                          .broadcast((neta, neta, nphi, nphi, nzvtx))
+                          .broadcast((neta, neta, nphi, nphi, nzvtx, nmult))
                           .expect("Can't broadcast")) *
                      (&self.singles
                           .counts
                           .to_owned()
-                          .into_shape((neta, 1, nphi, 1, nzvtx))
+                          .into_shape((neta, 1, nphi, 1, nzvtx, nmult))
                           .expect("Can't reshape"));
 
         // * 2, since the folded single distributions are a "double count"
         &self.pairs.counts / &phiphi * &self.event_counter.counts * 2.0
     }
 
-    fn get_uncert_dphi(&self) -> nd::Array<f64, nd::Dim<[usize; 1]>> {
-        let nphi = 20;
-        let neta = 16;
-        let nzvtx = 8;
+    /// Get the uncertainties on the dphi projection as shape (dphi, multiplicity)
+    fn get_uncert_dphi(&self) -> nd::Array<f64, nd::Dim<[usize; 2]>> {
+        let shape = self.singles.counts.shape();
+        let (neta, nphi, nzvtx, nmult) = (shape[0], shape[1], shape[2], shape[3]);
         let ss = (&self.singles
                   .counts
                   .to_owned()
-                  .into_shape((1, neta, 1, nphi, nzvtx))
+                  .into_shape((1, neta, 1, nphi, nzvtx, nmult))
                   .expect("Can't reshape")
-                  .broadcast((neta, neta, nphi, nphi, nzvtx))
+                  .broadcast((neta, neta, nphi, nphi, nzvtx, nmult))
                   .expect("Can't broadcast")) *
             (&self.singles
              .counts
              .to_owned()
-             .into_shape((neta, 1, nphi, 1, nzvtx))
+             .into_shape((neta, 1, nphi, 1, nzvtx, nmult))
              .expect("Can't reshape"));
         let ss_sum = ss.sum(Axis(4)).sum(Axis(0)).sum(Axis(0));
         let ss_sum = roll_diagonal(&ss_sum);
@@ -99,10 +104,13 @@ impl ParticlePairDistributions {
 impl ProcessEvent for ParticlePairDistributions {
     fn process_event(&mut self, sel_event: &Event, sel_tracks: &[&Track]) {
         // Fill only if we have a valid z-vtx position
+        let multiplicity = sel_event.multiplicity as f64;
         if let Some(ref pv) = sel_event.primary_vertex {
             self.singles
-                .extend(sel_tracks.iter().map(|tr| [tr.eta(), tr.phi(), pv.z]));
-            self.event_counter.fill(&[pv.z]);
+                .extend(sel_tracks
+                        .iter()
+                        .map(|tr| [tr.eta(), tr.phi(), pv.z, multiplicity]));
+            self.event_counter.fill(&[pv.z, multiplicity]);
 
             // Convert to indices before the nested loop; relies on
             // the fact that the hist is square in eta-eta and phi-phi
@@ -115,7 +123,8 @@ impl ProcessEvent for ParticlePairDistributions {
                 .filter_map(|tr|
                             [self.pairs.find_bin_index_axis(0, tr.eta()),
                              self.pairs.find_bin_index_axis(2, tr.phi()),
-                             self.pairs.find_bin_index_axis(4, pv.z),]
+                             self.pairs.find_bin_index_axis(4, pv.z),
+                             self.pairs.find_bin_index_axis(5, multiplicity),]
                             .into_iter()
                             .cloned()
                             .collect::<Option<Vec<usize>>>()
@@ -132,7 +141,7 @@ impl ProcessEvent for ParticlePairDistributions {
                         .map(move |(_, tr2)| {
                             [tr1[0], tr2[0],
                              tr1[1], tr2[1],
-                             tr1[2]]
+                             tr1[2], tr1[3]]
                         })
                 });
             for idxs in pair_idxs.into_iter() {
@@ -209,9 +218,10 @@ impl Visualize for ParticlePairDistributions {
     }
 }
 
-fn roll_diagonal(a: &nd::Array2<f64>) -> nd::Array2<f64>
+fn roll_diagonal<D>(a: &nd::Array<f64, D>) -> nd::Array<f64, D>
+    where D: nd::Dimension + nd::RemoveAxis
 {
-    let mut b = a.clone().reversed_axes();
+    let mut b = a.clone();
     for (i, mut row) in b.outer_iter_mut().enumerate() {
         for _ in 0..i {
             roll_by_one(&mut row);
@@ -220,20 +230,27 @@ fn roll_diagonal(a: &nd::Array2<f64>) -> nd::Array2<f64>
     b
 }
 
-fn roll_by_one<'a>(a: &mut nd::ArrayViewMut1<'a, f64>){
+fn roll_by_one<'a, D>(a: &mut nd::ArrayViewMut<'a, f64, D>)
+    where D: nd::Dimension
+{
     use std::mem;
-    let len = a.len();
-    let mut last = a[len - 1];
-    for elt in a.slice_mut(s![..-1;-1]) {
-        last = mem::replace(elt, last);
+    for mut lane in a.lanes_mut(nd::Axis(0)) {
+        let len = lane.len();
+        let mut last = lane[len - 1];
+        for elt in lane.slice_mut(s![..-1;-1]) {
+            last = mem::replace(elt, last);
+        }
+        lane[len - 1] = last;
     }
-    a[len - 1] = last;
 }
 
-fn get_phi_phi(a: &nd::Array5<f64>) -> nd::Array2<f64> {
+/// Average the given array of shape (eta, eta, phi, phi, z, mult) to
+/// (phi, phi, mult)
+fn get_phi_phi(a: &nd::Array6<f64>) -> nd::Array3<f64> {
     nanmean(&nanmean(&nanmean(a, 4), 0), 0)
 }
 
+/// Fourier decompose a 1D distribution
 fn fourier_decompose(a: &nd::Array1<f64>) -> Vec<Complex<f64>> {
     let mut input: Vec<Complex<f64>> = a
         .to_vec()
@@ -251,16 +268,16 @@ fn fourier_decompose(a: &nd::Array1<f64>) -> Vec<Complex<f64>> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn fourier() {
-        let n = 5.;
-        let a = nd::Array1::<f64>::range(0., 2. * PI, 2.0 * PI / n)
-            .mapv(|v| v.cos());
+    // #[test]
+    // fn fourier() {
+    //     let n = 5.;
+    //     let a = nd::Array1::<f64>::range(0., 2. * PI, 2.0 * PI / n)
+    //         .mapv(|v| v.cos());
 
-        let output = fourier_decompose(&a);
-        println!("amp: {:?}", output.iter().map(|v| v.to_polar().0).collect::<Vec<_>>());
-        println!("phase: {:?}", output.iter().map(|v| v.to_polar().1).collect::<Vec<_>>());
-    }
+    //     let output = fourier_decompose(&a);
+    //     println!("amp: {:?}", output.iter().map(|v| v.to_polar().0).collect::<Vec<_>>());
+    //     println!("phase: {:?}", output.iter().map(|v| v.to_polar().1).collect::<Vec<_>>());
+    // }
 
     #[test]
     fn test_roll_by_one() {
@@ -271,16 +288,32 @@ mod tests {
 
     #[test]
     fn roll_2d_by_one() {
-        let mut a = nd::Array2::<f64>::zeros((4, 4)); // nd::Array::linspace(0., 9., 9).into_shape((3, 3));
+        let mut a = nd::Array2::<f64>::zeros((3, 3)); // nd::Array::linspace(0., 9., 9).into_shape((3, 3));
         for (i, el) in a.iter_mut().enumerate() {
             *el = i as f64;
         }
-        println!("{:?}", a);
         for (i, mut row) in a.outer_iter_mut().enumerate() {
             for _ in 0..i {
                 roll_by_one(&mut row);
             }
         }
-        println!("{:?}", a);
+        let res: nd::Array2<f64> = nd::arr2(&[[0., 1., 2.],
+                                              [4., 5., 3.],
+                                              [8., 6., 7.]]);
+        assert_eq!(a, res);
+    }
+
+    #[test]
+    fn test_roll_diagonal() {
+        let mut a = nd::Array2::<f64>::zeros((3, 3));
+        for (i, el) in a.iter_mut().enumerate() {
+            *el = i as f64;
+        }
+        let a = a.into_shape((3, 3, 1)).unwrap();
+        let rolled = roll_diagonal(&a);
+        let res = nd::arr2(&[[0., 1., 2.],
+                              [4., 5., 3.],
+                              [8., 6., 7.]]).into_shape((3, 3, 1)).unwrap();
+        assert_eq!(rolled, res);
     }
 }
