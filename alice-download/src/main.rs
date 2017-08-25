8b0ed83fdd130a90285b2ec165c0d8273ed0f97a
cbourjau <christian.bourjau@cern.ch>
2017-08-25 16:42:15 +0200

    split alic-open-data into a lib and a bin

--- alice-download/src/main.rs
+++ alice-download/src/main.rs
@@ -1,20 +1,15 @@
-extern crate reqwest;
 extern crate indicatif;
-
+#[macro_use]
+extern crate clap;
 extern crate alice_open_data;
-
-use std::fs::{File, DirBuilder};
-use std::io::copy;
 
 use indicatif::ProgressBar;
 use clap::{Arg, App};
 use alice_open_data::*;
 
-#[macro_use]
-extern crate clap;
 
 fn main() {
-    let matches = App::new("alice-open-data")
+    let matches = App::new("alice-download")
         .version("0.1")
         .about("Download specified amount of ALICE open Pb--Pb data to ~/lhc_open_data \
                 Visit `http://opendata.cern.ch/search?cc=ALICE-Reconstructed-Data` for \
@@ -38,7 +33,7 @@ fn main() {
         139_465,
     ];
     let files: Vec<FileDetails> = runs.iter()
-        .flat_map(|r| get_dataset_info(*r).unwrap().file_details)
+        .flat_map(|r| file_details_of_run(*r).unwrap())
         .collect();
     let total_size = files.iter().fold(0, |acc, v| acc + v.file_size) as f64 / 1e9;
     println!("Total available data: {} files with total of {} GB", files.len(), total_size);
@@ -59,23 +54,7 @@ fn main() {
     let max_files = pbar.wrap_iter(max_files.iter());
 
     for f in max_files {
-        let url = "http://opendata.cern.ch".to_string() + &f.file_path;
-        let mut dest = data_dir();
-        let mut sub_dir = f.original_filename.to_owned();
-        // Remove the leading "\" from the original path
-        sub_dir.remove(0);
-        dest.push(sub_dir);
-        // Do not re-download if the file already exists
-        if dest.exists() {
-            continue;
-        }
-        // Make sure the dir exists
-        if let Some(dir) = dest.parent() {
-            DirBuilder::new().recursive(true).create(dir).unwrap();
-        }
-        let mut f = File::create(dest).unwrap();
-        let mut resp = reqwest::get(&url).expect("Could not read file");
-        copy(&mut resp, &mut f).unwrap();
+        f.download().unwrap();
     }
 }
 
--- alice-open-data/src/lib.rs
+++ alice-open-data/src/lib.rs
@@ -5,54 +5,85 @@ extern crate serde_derive;
 extern crate reqwest;
 
 use std::env;
-use std::io::{Read};
+use std::io::{self, Read};
 use std::path::PathBuf;
+use std::fs::{File, DirBuilder};
+use std::io::copy;
+
 
 /// The json data of the datasets is wrapped in an object with key "Dataset"
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct SillyWrapper {
+struct SillyWrapper {
     dataset: Dataset
 }
 
 /// Summary information of the given dataset/run
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Dataset {
+struct Dataset {
     name: String,
     description: String,
     path: String,
     files: i32,
-    pub file_details: Vec<FileDetails>,
+    file_details: Vec<FileDetails>,
 }
 
 /// Details about one specific file in a given run
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileDetails {
-    pub file_path: String,
+    file_path: String,
     pub file_size: i64,
     file_checksum: String,
     file_timestamp: String,
-    pub original_filename: String,
+    original_filename: String,
 }
 
+impl FileDetails {
+    /// The url pointing to the current file
+    pub fn url(&self) -> String {
+        let url = "http://opendata.cern.ch".to_string();
+        url + &self.file_path
+    }
+
+    /// Download the current file to the local collection
+    pub fn download(&self) -> Result<(), Error> {
+        let mut dest = data_dir()?;
+        let mut sub_dir = self.original_filename.to_owned();
+        // Remove the leading "\" from the original path
+        sub_dir.remove(0);
+        dest.push(sub_dir);
+        // Do not re-download if the file already exists
+        if dest.exists() {
+            return Ok(());
+        }
+        // Make sure the dir exists
+        if let Some(dir) = dest.parent() {
+            DirBuilder::new().recursive(true).create(dir)?;
+        }
+        let mut f = File::create(dest)?;
+        let mut resp = reqwest::get(&self.url())?;
+        copy(&mut resp, &mut f)?;
+        Ok(())
+    }
+}
 
 /// Base path to the local ALICE open data directory
-pub fn data_dir() -> PathBuf {
-    let mut dir = env::home_dir().expect("Cannot find user's home directory");
+pub fn data_dir() -> Result<PathBuf, Error> {
+    let mut dir = env::home_dir().ok_or(Error::NoHomeDir)?;
     dir.push("lhc_open_data");
-    dir
+    Ok(dir)
 }
 
 /// Hardcoded path to a specific file. Useful for testing.
 /// That file should be the the first to be downloaded automatically.
-pub fn test_file() -> PathBuf {
-    let mut dir = data_dir();
+pub fn test_file() -> Result<PathBuf, Error> {
+    let mut dir = data_dir()?;
     dir.push("alice/data/2010/LHC10h/000139038/ESDs/pass2/10000139038001.10/AliESDs.root");
-    dir
+    Ok(dir)
 }
 
 /// Get the details for a specific dataset
-pub fn get_dataset_info(run: u32) -> Result<Dataset, reqwest::Error> {
+pub fn file_details_of_run(run: u32) -> Result<Vec<FileDetails>, Error> {
     // Runs are blocks of data taking
     // One URL per run
     let urls = [
@@ -69,14 +100,39 @@ pub fn get_dataset_info(run: u32) -> Result<Dataset, reqwest::Error> {
     reqwest::get(*url)?
     .read_to_string(&mut content)
         .expect("Could not read to string");
-    let wrapper: SillyWrapper = serde_json::from_str(&content).unwrap();
-    Ok(wrapper.dataset)
+    let wrapper: SillyWrapper = serde_json::from_str(&content)?;
+    Ok(wrapper.dataset.file_details)
 }
 
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    HttpError(reqwest::Error),
+    ParseJson(serde_json::Error),
+    NoHomeDir,
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Error {
+        Error::HttpError(err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Error {
+        Error::ParseJson(err)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::{SillyWrapper, File};
+    use super::{SillyWrapper, FileDetails};
     use serde_json;
 
     #[test]
@@ -108,7 +164,7 @@ mod tests {
       "file_timestamp": "2013-01-17 15:01:44",
       "original_filename": "/alice/data/2010/LHC10h/000139173/ESDs/pass2/10000139173001.10/AliESDs.root"
       }"#;
-        let ds: File = serde_json::from_str(data).unwrap();
+        let _ds: FileDetails = serde_json::from_str(data).unwrap();
     }    
 
 }
