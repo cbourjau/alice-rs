db898229d342cada2a2158dcee97109dc104c03a
cbourjau <christian.bourjau@cern.ch>
2018-02-19 12:14:23 +0100

    fixes to `simple-analysis`

--- simple-analysis/src/main.rs
+++ simple-analysis/src/main.rs
@@ -39,11 +39,9 @@ fn main() {
 /// For that, the returned object must implementent `alice::Merge`
 fn single_distribution_analysis(events: Box<Iterator<Item=Event>>) -> distribution::Distribution {
     events
-    // Event selection
+        // Event selection
         .filter(default_event_filter)
-    // Track selection
-    // .map(filter_tracks)
-    // Analysis; Fold this chunk of events
+        // Analysis; Fold this chunk of events
         .fold(distribution::Distribution::new(), |analysis, ev| {
             analysis.process_event(&ev)
         })
@@ -64,59 +62,5 @@ pub fn default_event_filter(event: &Event) -> bool {
     } else {
         return false;
     }
-    // Require some activity in the central region
-    // if event.multiplicity <= 0 {
-    //     return false;
-    // }
-    // Only use events which fired the minimu bias trigger
-    // if !event.trigger_mask.contains(trigger_mask::MINIMUM_BIAS) {
-    //     return false;
-    // }
     true
 }
-
-// /// Apply default track selection cuts
-// /// The returned Event contains only those tracks that passed the cuts
-// /// The cuts are partly inspired by those define around
-// /// AliESDtrackCuts.cxx:1366
-// pub fn filter_tracks(mut ev: Event) -> Event {
-//     {
-//         let pv = ev.primary_vertex().expect("No primary vertex for found!");
-//         // see AliESDtrackCuts.cxx:1366
-//         // ev.tracks = ev.tracks()
-//         // Part of the SPD were turned off due to cooling issues in
-//         // LHC10h data-taking In order to keep a flat acceptance in
-//         // phi and eta, we allow tracks without a hit in the SPD to
-//         // _not_ have an ITS refit The expense of this looser cut is
-//         // an increased number of secondary particles
-//             // .filter(|tr| {
-//             //     // SPD && ITS_REFIT
-//             //     (tr.quality_its.clusters_on_layer.intersects(
-//             //         track::SPD_INNER | track::SPD_OUTER)
-//             //      & tr.flags.contains(track::ITS_REFIT)) ||
-//             //     // !SPD && ITS_REFIT
-//             //     (!tr.quality_its.clusters_on_layer.intersects(
-//             //         track::SPD_INNER | track::SPD_OUTER)
-//             //      & tr.flags.contains(track::ITS_REFIT)) ||
-//             //     // !SPD && !ITS_REFIT
-//             //     (!tr.quality_its.clusters_on_layer.intersects(
-//             //         track::SPD_INNER | track::SPD_OUTER)
-//             //      & !tr.flags.contains(track::ITS_REFIT))
-//         // })
-//             .filter(|tr| tr.flags.contains(track::Flags::ITS_REFIT))
-//             .filter(|tr| tr.flags.contains(track::Flags::TPC_REFIT))
-//             .filter(|tr| tr.dca_to_point_xy(pv.x, pv.y) < 2.4)
-//             .filter(|tr| tr.dca_to_point_z(pv.z) < 3.2)
-//             .filter(|tr| tr.eta().abs() < 0.9)
-
-//             .filter(|tr| tr.pt() > 0.15)
-//             // .filter(|tr| tr.quality_tpc.n_clusters > 70)
-//             // .filter(|tr| tr.quality_tpc.chi2_per_cluster() <= 4.0)
-//             // .filter(|tr| tr.quality_its.chi2_per_cluster() <= 36.0)
-//             .collect();
-//     }
-//     // Shuffle selected tracks to avoid correlations from datataking orderings
-//     // Trust me, this is needed!
-//     // thread_rng().shuffle(ev.tracks.as_mut_slice());
-//     ev
-// }
