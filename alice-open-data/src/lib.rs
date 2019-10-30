79b068f42b6aa4e011c4364b4d4ad41e4b06bd4c
cbourjau <cbourjau@posteo.de>
2019-10-30 15:22:49 +0100

    open-data: add passing and meaningful tests for wasm and x86
    The required patching reqwest to set the headers and to get to the
    bytes of the wasm requests/responses.

--- alice-open-data/src/lib.rs
+++ alice-open-data/src/lib.rs
@@ -1,12 +1,20 @@
 #[cfg(not(target_arch = "wasm32"))]
-use std::fs::{DirBuilder, File};
-use std::io::Write;
-use std::path::PathBuf;
+use std::{
+    fs::{DirBuilder, File},
+    io::Write,
+    path::PathBuf,
+};
 
-#[cfg(target_arch = "wasm32")]
-use futures::FutureExt;
 use failure::{Error, format_err};
-use reqwest::{Client, Response, Url};
+use reqwest::{Client, Url};
+
+fn root_url() -> Url {
+    if !cfg!(target_arch = "wasm32") {
+        "http://opendata.cern.ch/"
+    } else {
+        "http://cirrocumuli.com/"
+    }.parse().unwrap()
+}
 
 /// Download the given file to the local collection
 #[cfg(not(target_arch = "wasm32"))]
@@ -24,7 +32,7 @@ pub async fn download(base_dir: PathBuf, url: Url) -> Result<usize, Error> {
     if let Some(dir) = dest.parent() {
         DirBuilder::new().recursive(true).create(dir)?;
     }
-    let resp = download_with_https(url).await?;
+    let resp = Client::new().get(url).send().await?;
     let bytes: Vec<_> = resp.bytes().await?.into_iter().collect();
     let mut f = File::create(dest)?;
     Ok(f.write(&bytes)?)
@@ -60,48 +68,38 @@ pub fn all_files_10h() -> Result<Vec<PathBuf>, Error> {
 }
 
 pub async fn get_file_list(run: u32) -> Result<Vec<Url>, Error> {
-    #[cfg(not(target_arch = "wasm32"))]
-    let uri = "http://opendata.cern.ch/record/".to_owned()
-        + match run {
-            139_038 => "1102/files/ALICE_LHC10h_PbPb_ESD_139038_file_index.txt",
-            139_173 => "1103/files/ALICE_LHC10h_PbPb_ESD_139173_file_index.txt",
-            139_437 => "1104/files/ALICE_LHC10h_PbPb_ESD_139437_file_index.txt",
-            139_438 => "1105/files/ALICE_LHC10h_PbPb_ESD_139438_file_index.txt",
-            139_465 => "1106/files/ALICE_LHC10h_PbPb_ESD_139465_file_index.txt",
-            _ => return Err(format_err!("Invalid run number")),
-        };
-    #[cfg(target_arch = "wasm32")]
-    let uri = "http://cirrocumuli.com/ALICE_LHC10h_PbPb_ESD_139038_file_index.txt".to_string();
-    let req = Client::new()
-        .get(uri.as_str());
+    // Do to CORS we have to get change the urls based on the target for now
+    
+    let uri = "http://opendata.cern.ch/".parse::<Url>()?.join(
+        if !cfg!(target_arch = "wasm32") {
+            match run {
+                139_038 => "record/1102/files/ALICE_LHC10h_PbPb_ESD_139038_file_index.txt",
+                139_173 => "record/1103/files/ALICE_LHC10h_PbPb_ESD_139173_file_index.txt",
+                139_437 => "record/1104/files/ALICE_LHC10h_PbPb_ESD_139437_file_index.txt",
+                139_438 => "record/1105/files/ALICE_LHC10h_PbPb_ESD_139438_file_index.txt",
+                139_465 => "record/1106/files/ALICE_LHC10h_PbPb_ESD_139465_file_index.txt",
+                _ => return Err(format_err!("Invalid run number")),
+            }
+        } else {
+            "http://cirrocumuli.com/ALICE_LHC10h_PbPb_ESD_139038_file_index.txt"
+        }
+    )?;
+
+    let req = Client::new().get(uri);
     let resp = req.send().await?;
     if resp.status().is_success() {
         let content = resp.text().await?;
-        Ok(content
+        content
             .lines()
-            .map(|l| format!("http://opendata.cern.ch/{}", &l[26..]))
-            .map(|l| l.parse::<Url>().expect("Invalid file URI"))
-            .collect())
+            .map(|l| root_url()
+                 .join(&l[26..])
+                 .map_err(Into::into)
+            )
+            .collect()
     } else {
         Err(format_err!("Could not download list of files"))
     }
 }
-
-async fn download_with_https(uri: Url) -> Result<Response, Error> {
-    return Ok(Client::new().get(uri).send().await?);
-}
-
-// #[cfg(target_arch = "wasm32")]
-// fn download_with_https(uri: Url) -> Result<reqwest::Response, Error> {
-//     use std::sync::mpsc::channel;
-//     let (rx, tx) = channel();
-//     let future = Client::new()
-//         .get(uri)
-//         .send()
-//         .map(move |res| rx.send(res.unwrap()).unwrap());
-//     wasm_bindgen_futures::spawn_local(future);
-//     Ok(tx.recv()?)
-// }
 
 #[cfg(not(target_arch = "wasm32"))]
 #[cfg(test)]
@@ -116,21 +114,23 @@ mod tests_x84 {
         let client = Client::builder()
             .build()
             .unwrap();
+        let url = root_url().join("/eos/opendata/alice/2010/LHC10h/000139038/ESD/0001/AliESDs.root").unwrap();
+        let (start, len) = (13993603, 68936);
         let rsp = client
-        // .get("https://eospublichttp.cern.ch/eos/opendata/alice/2010/LHC10h/000139038/ESD/0001/AliESDs.root")
-            .get("http://opendata.cern.ch//eos/opendata/alice/2010/LHC10h/000139038/ESD/0001/AliESDs.root")
+            .get(url)
             .header("User-Agent", "alice-rs")
-            .header(RANGE, "bytes=0-1023")
+            .header(RANGE, &format!("bytes={}-{}", start, start + len -1))
             .send().await.unwrap();
         dbg!(&rsp);
 
         let partial = rsp.bytes().await.unwrap();
-        assert_eq!(partial.len(), 1024);
+        assert_eq!(partial.len(), len);
         #[cfg(not(target_arch = "wasm32"))]
         {
             let from_disc = std::fs::read(test_file().unwrap()).unwrap();
             assert!(
                 partial.iter()
+                    .skip(start)
                     .zip(from_disc.iter())
                     .all(|(el1, el2)| el1 == el2)
             );
@@ -149,7 +149,7 @@ mod tests_x84 {
     #[tokio::test]
     async fn test_download_file() {
         let uris = &super::get_file_list(139038).await.unwrap();
-        let resp = super::download_with_https(uris[0].clone()).await.unwrap();
+        let resp = Client::new().get(uris[0].clone()).send().await.unwrap();
         println!("{:?}", resp);
         println!("{:?}", uris[0].path());
     }
