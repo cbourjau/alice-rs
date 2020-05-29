9a29198ee791f1a7e5d280dd8f752d1bfa3fc15b
cbourjau <cbourjau@posteo.de>
2020-05-29 22:23:58 +0200

    Update example

--- examples/simple-analysis/src/distribution.rs
+++ examples/simple-analysis/src/distribution.rs
@@ -12,9 +12,9 @@ use malice::default_track_filter;
 use malice::Event;
 
 pub struct SimpleAnalysis {
-    pub single_particles: Histogram<f32, [usize; 3]>,
-    pub z_vertex: Histogram<i32, [usize; 1]>,
-    pub multiplicity: Histogram<f32, [usize; 1]>,
+    pub single_particles: Histogram,
+    pub z_vertex: Histogram,
+    pub multiplicity: Histogram,
 }
 
 impl SimpleAnalysis {
@@ -25,17 +25,17 @@ impl SimpleAnalysis {
         let nmult = 3000;
         let (nzvtx, zmin, zmax) = (100, -10., 10.);
         SimpleAnalysis {
-            single_particles: HistogramBuilder::<[usize; 3]>::new()
+            single_particles: HistogramBuilder::new()
                 .add_equal_width_axis(neta, -0.8, 0.8)
                 .add_equal_width_axis(nphi, 0., 2. * PI)
                 .add_equal_width_axis(nzvtx, zmin, zmax)
                 .build()
                 .expect("Error building histogram"),
-            z_vertex: HistogramBuilder::<[usize; 1]>::new()
+            z_vertex: HistogramBuilder::new()
                 .add_equal_width_axis(nzvtx, zmin, zmax)
                 .build()
                 .expect("Error building histogram"),
-            multiplicity: HistogramBuilder::<[usize; 1]>::new()
+            multiplicity: HistogramBuilder::new()
                 .add_equal_width_axis(nmult, 0.0, nmult as f64)
                 .build()
                 .expect("Error building histogram"),
@@ -47,18 +47,14 @@ impl SimpleAnalysis {
     pub fn process_event(&mut self, event: &Event) {
         // Fill only if we have a valid primary vertex
         if let Some(prime_vtx) = event.primary_vertex() {
-            self.single_particles.extend(
-                event
-                    .tracks()
-                    .filter(|tr| default_track_filter(&tr, &prime_vtx))
-                    .map(|tr| {
-                        [
-                            f64::from(tr.eta()),
-                            f64::from(tr.phi()),
-                            f64::from(prime_vtx.z),
-                        ]
-                    }),
-            );
+	    for track in event.tracks().filter(|tr| default_track_filter(&tr, &prime_vtx)) {
+		self.single_particles.fill(&[
+                    f64::from(track.eta()),
+                    f64::from(track.phi()),
+                    f64::from(prime_vtx.z),
+                ])
+	    }
+
             self.z_vertex.fill(&[f64::from(prime_vtx.z)]);
             self.multiplicity.fill(&[event
                 .tracks()
@@ -79,7 +75,7 @@ impl SimpleAnalysis {
     /// Visualized the data using gnuplot-rs
     pub fn visualize(&self) {
         let mut fg = Figure::new();
-        let eta_bin_width = self.single_particles.widths(0)[0] as f32;
+        let eta_bin_width: f64 = self.single_particles.widths(0)[0];
         let plot_options = [PlotOption::Color("#d95f02"), PlotOption::FillAlpha(0.8)];
         fg.axes2d()
             .set_pos_grid(2, 2, 0)
@@ -89,18 +85,18 @@ impl SimpleAnalysis {
             .boxes(
                 &self.single_particles.centers(0),
                 // Sum over phi and z
-                (&self
-                    .single_particles
-                    .counts
-                    .sum_axis(Axis(1))
-                    .sum_axis(Axis(1))
-                    / self.z_vertex.counts.scalar_sum() as f32
-                    / eta_bin_width)
-                    .view(),
+		self.single_particles
+		    .sum_axis(1)
+		    .sum_axis(1)
+		    .mul(
+			1.0
+			    / self.z_vertex.values().iter().sum::<f64>()
+			    / eta_bin_width
+		    ).values().into_iter(),
                 &plot_options,
             );
 
-        let phi_bin_width = self.single_particles.widths(1)[0] as f32;
+        let phi_bin_width = self.single_particles.widths(1)[0];
         let x_ticks = vec![
             Tick::Major(0.0, AutoOption::Fix("0".to_owned())),
             Tick::Major(0.5 * PI, AutoOption::Fix("0.5 π".to_owned())),
@@ -119,14 +115,14 @@ impl SimpleAnalysis {
             .boxes(
                 &self.single_particles.centers(1),
                 // Sum over eta and z
-                (&self
+                self
                     .single_particles
-                    .counts
-                    .sum_axis(Axis(2))
-                    .sum_axis(Axis(0))
-                    / self.z_vertex.counts.scalar_sum() as f32
-                    / phi_bin_width)
-                    .view(),
+                    .sum_axis(2)
+                    .sum_axis(0)
+		    .mul(1.0
+			 / self.z_vertex.values().iter().sum::<f64>()
+			 / phi_bin_width
+		    ).values().into_iter(),
                 &plot_options,
             );
 
@@ -137,7 +133,7 @@ impl SimpleAnalysis {
             .set_y_label("# events", &[])
             .boxes(
                 &self.z_vertex.centers(0),
-                &self.z_vertex.counts,
+                self.z_vertex.values().iter(),
                 &plot_options,
             );
 
@@ -150,7 +146,7 @@ impl SimpleAnalysis {
             .set_y_log(Some(10.0))
             .boxes(
                 &self.multiplicity.centers(0),
-                &self.multiplicity.counts,
+                self.multiplicity.values().iter(),
                 &plot_options,
             );
         fg.show();
@@ -158,10 +154,10 @@ impl SimpleAnalysis {
 
     /// Compute the centrality edges based on the N_ch/Event distribution
     pub fn compute_centrality_edges(&self) {
-        let tot = self.multiplicity.counts.scalar_sum();
+        let tot: f64 = self.multiplicity.values().iter().sum();
         let cum: Vec<_> = self
             .multiplicity
-            .counts
+            .values()
             .iter()
             .scan(0.0, |state, el| {
                 *state += el;
@@ -172,10 +168,10 @@ impl SimpleAnalysis {
             // Bin width is one track, so we just enumerate to have the number of tracks
             .enumerate()
             .collect::<Vec<_>>();
-        let percent_edges = (1..11)
+        let percent_edges = (1..=10)
             .rev()
             .filter_map(|v| {
-                let want_this_percent = (v * 10) as f32;
+                let want_this_percent = (v * 10) as f64;
                 cum.iter()
                     .find(|bin_percent| bin_percent.1 <= want_this_percent)
             })
